4d5851cb261007454c7ea45e3e5600bac83502b3
Jason Neylon <jason.neylon@uswitch.com>
2015-10-12 22:25:20 +0100

    Almost screaming

--- Main.elm
+++ Main.elm
@@ -10,6 +10,7 @@ import Keyboard exposing (..)
 import Html exposing (Html, fromElement, Attribute, text, toElement, div, input, h1, h2, node, button)
 import Html.Attributes exposing (..)
 import Html.Events exposing (on, targetValue)
+import Signal exposing (Address, mailbox)
 
 --import StartApp.Simple exposing (start)
 
@@ -17,7 +18,7 @@ type alias Model = String
 
 main : Signal Html
 main = 
-  Signal.map3 renderScene Window.dimensions (every (50 * millisecond)) (timestamp Keyboard.enter)
+  Signal.map4 renderScene myMailbox.signal (every (50 * millisecond)) (timestamp Keyboard.enter) Window.dimensions
   --(fpsWhen 5 (Signal.constant True))
 
 
@@ -31,45 +32,57 @@ heading =
   , h2 [style [("text-align", "center"), ("margin", "0"), ("color", "white"), ("font-size", "48pt"), ("font-family", "Creepster")]] [Html.text "into the void"]
   ]
 
-inputArea : Html
-inputArea =
-  --fromElement (container 300 300 middle (show "Try this with html."))
-  div [style [("position", "absolute"),
-          ("z-index", "100"),
-          ("width", "100%"),
-          ("height", "100%"),
-          ("display", "flex"),
-          ("justify-content", "center"),
-          ("align-items", "center")
-          ]] 
-  [
-    input [style []] []
-  , button [style []] [Html.text "Scream!"]
-  ]
+inputArea : String -> Bool -> Html
+inputArea message show =
+  let 
+    inputStyles = if show then [("display", "none")] else [("display", "block"), ("width", "400px"), ("height", "100px"), ("font-size", "32pt")]
+  in
+    --fromElement (container 300 300 middle (show "Try this with html."))
+    div [style [("position", "absolute"),
+            ("z-index", "100"),
+            ("width", "100%"),
+            ("height", "100%"),
+            ("display", "flex"),
+            ("justify-content", "center"),
+            ("align-items", "center")
+            ]] 
+    [
+      input [ style inputStyles
+            , placeholder "What's up?"
+            , value message
+            , on "input" targetValue (Signal.message myMailbox.address)] 
+          []
+    --, button [style []] [Html.text "Scream!"]
+    ]
 
 css : String -> Html
 css path =
   node "link" [ rel "stylesheet", href path ] []
 
-renderScene: (Int, Int) -> Float -> (Float, a) -> Html
-renderScene (width, height) time (enterPressed, _) =
+renderScene: String -> Float -> (Float, a) -> (Int, Int) -> Html
+renderScene message time (enterPressed, _) (width, height) =
   let 
     angle = (toFloat ((round (6 * inSeconds time)) % 360))
     sinceEnter = round ((time - enterPressed) / 100)
     shrinkingHeight = toFloat (72 - (3 * sinceEnter))
     spinningAwayAngle = toFloat (60 + (24 * sinceEnter))
+    showInput = (sinceEnter < 40)
+    spinnyMessage = if showInput then message else ""
   in
     --log ("delay: " ++ (toString (inSeconds delay))) <|
     div [] 
     [
       css "https://fonts.googleapis.com/css?family=Creepster"
     ,  heading
-    ,  inputArea
+    ,  inputArea message showInput
     ,  fromElement (collage width height
       [ 
         rect (toFloat width) (toFloat height) |> filled black |> move(0, 0)
       , rotate (degrees angle) (toForm (image (width * 2) (height * 2) "/images/void_compressed.jpg"))
-      , rotate (degrees spinningAwayAngle) (toForm (centered (Text.height shrinkingHeight  (color white (fromString ("Message will go here " ++ (toString sinceEnter)))))))
-      , rotate (degrees angle) (toForm (centered (color red (fromString ("Scream into the void : " ++ (toString angle))))))
+      , rotate (degrees spinningAwayAngle) (toForm (centered (Text.height shrinkingHeight (color white (fromString spinnyMessage)))))
+      --, rotate (degrees angle) (toForm (centered (color red (fromString ("Scream into the void : " ++ (toString angle))))))
       ])
     ]
+
+myMailbox : { address : Address String, signal : Signal String }
+myMailbox = Signal.mailbox ""
