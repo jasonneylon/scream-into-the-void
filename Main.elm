55daeb72acb4ea014b08e752a32da508f731cded
Jason Neylon <jason.neylon@uswitch.com>
2015-10-12 21:56:06 +0100

    Added an input area

--- Main.elm
+++ Main.elm
@@ -1,32 +1,50 @@
 
 import Graphics.Collage exposing (..)
-import Graphics.Element exposing (image, fittedImage, croppedImage, Element, centered)
+import Graphics.Element exposing (image, fittedImage, croppedImage, Element, centered, show, middle, container)
 import Color exposing (red, black, white)
 import Window
 import Time exposing (..)
 import Text exposing (color, fromString)
 import Debug exposing ( log )
 import Keyboard exposing (..)
-
-
-import Html exposing (Html, fromElement, Attribute, text, toElement, div, input, h1, h2, node)
+import Html exposing (Html, fromElement, Attribute, text, toElement, div, input, h1, h2, node, button)
 import Html.Attributes exposing (..)
 import Html.Events exposing (on, targetValue)
 
+--import StartApp.Simple exposing (start)
+
+type alias Model = String
 
 main : Signal Html
 main = 
   Signal.map3 renderScene Window.dimensions (every (50 * millisecond)) (timestamp Keyboard.enter)
   --(fpsWhen 5 (Signal.constant True))
 
+
 heading: Html
 heading =
   div [style [("position", "absolute"),
-        ("z-index", "100"),
-        ("width", "100%")]] 
+          ("z-index", "100"),
+          ("width", "100%")]] 
   [
     h1 [style [("text-align", "center"), ("margin", "0"), ("color", "white"), ("font-size", "96pt"), ("font-family", "Creepster")]] [Html.text "Scream"]
   , h2 [style [("text-align", "center"), ("margin", "0"), ("color", "white"), ("font-size", "48pt"), ("font-family", "Creepster")]] [Html.text "into the void"]
+  ]
+
+inputArea : Html
+inputArea =
+  --fromElement (container 300 300 middle (show "Try this with html."))
+  div [style [("position", "absolute"),
+          ("z-index", "100"),
+          ("width", "100%"),
+          ("height", "100%"),
+          ("display", "flex"),
+          ("justify-content", "center"),
+          ("align-items", "center")
+          ]] 
+  [
+    input [style []] []
+  , button [style []] [Html.text "Scream!"]
   ]
 
 css : String -> Html
@@ -46,12 +64,11 @@ renderScene (width, height) time (enterPressed, _) =
     [
       css "https://fonts.googleapis.com/css?family=Creepster"
     ,  heading
+    ,  inputArea
     ,  fromElement (collage width height
       [ 
-        --toForm (h1 [] [Html.text "Scream!"])
         rect (toFloat width) (toFloat height) |> filled black |> move(0, 0)
       , rotate (degrees angle) (toForm (image (width * 2) (height * 2) "/images/void_compressed.jpg"))
-      --, rotate (degrees ((delay - 200.0) * 30)) (toForm (centered (Text.height (((delay - 200.0) + 1) * 8) (color white (fromString ("Message will go here " ++ (toString (delay - 200.0) )))))))
       , rotate (degrees spinningAwayAngle) (toForm (centered (Text.height shrinkingHeight  (color white (fromString ("Message will go here " ++ (toString sinceEnter)))))))
       , rotate (degrees angle) (toForm (centered (color red (fromString ("Scream into the void : " ++ (toString angle))))))
       ])
